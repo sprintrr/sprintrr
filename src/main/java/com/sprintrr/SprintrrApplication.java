05fee50aee0bbb871a0674e3f6a1d7ac1ea5954f
Prashant Kumar <prashant.ku@endurance.com>
2021-10-01 14:32:20 +0530

    Initializing spring boot project

---
+++ src/main/java/com/sprintrr/SprintrrApplication.java
@@ -0,0 +1,13 @@
+package com.sprintrr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintrrApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SprintrrApplication.class, args);
+  }
+
+}
